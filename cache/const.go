55d9cbc60f9ad0027a65676a6eb11b4b5de08d76
awen <zhiwen_der@qq.com>
2024-04-22 22:24:36 +0800

    完善注册功能

--- cache/const.go
+++ cache/const.go
@@ -9,15 +9,16 @@ const (
 	Separator = ":"
 
 	// PhoneLoginKey 用户短信登录验证码key
-	PhoneLoginKey = Prefix + Separator + "phone_login"
+	PhoneLoginKey = Prefix + Separator + "phone_login" + Separator
+	ForGetKey     = Prefix + Separator + "forget" + Separator
 
 	// PhoneSecureKey 更换手机绑定验证码key
-	PhoneSecureKey = Prefix + Separator + "phone_secure"
+	PhoneSecureKey = Prefix + Separator + "phone_secure" + Separator
 	// PhoneCodeTime 短信验证码登录时长
 	PhoneCodeTime = 30 * time.Second
 
 	// EmailVerifyKey  邮箱绑定验证缓存key
-	EmailVerifyKey = Prefix + Separator + "email_verify"
+	EmailVerifyKey = Prefix + Separator + "email_verify" + Separator
 	// EmailVerifyCodeTime 邮箱验证码缓存时长
 	EmailVerifyCodeTime = 30 * time.Minute
 )
